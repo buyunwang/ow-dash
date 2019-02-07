e381a9bd333169620731c43fa362a43b394e3d98
buyunwang <buyunwang@hotmail.com>
2019-02-06 22:11:27 -0800

    pull in stages

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import styled from 'styled-components';
+import AppLayout from './AppLayout';
+import AppBar from './AppBar';
+import {AppProvider} from './AppProvider';
+import {AppContext} from './AppProvider';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      Hello World
-      </div>
+      <AppLayout>
+      <AppProvider>
+        <h1>MATCH SCHEDULE</h1>
+          <AppBar />
+        </AppProvider>
+      </AppLayout> 
     );
   }
 }
